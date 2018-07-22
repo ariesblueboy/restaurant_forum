5ffb1987b3dc06f3f2871218f557a4622fa1e90f
Justin Chen <ariesblueboy@gmail.com>
2018-07-22 13:28:51 +0800

    setup seed data for default categories and admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Category
+
+Category.destroy_all
+
+category_list = [
+{name: "中式料理"},
+{name: "日式料理"},
+{name: "義大利料理"},
+{name: "墨西哥料理"},
+{name: "素食料理"},
+{name: "泰式料理"},
+{name: "複合式料理"}
+]
+
+category_list.each do |category|
+  Category.create(name: category[:name])
+end
+
+puts "category created!"
+
+# Default_admin
+
+User.create(email: "root@gmail.com", password: "love0809", role: "admin")
+puts "Admin created!"
