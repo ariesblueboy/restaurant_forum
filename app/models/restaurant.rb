94fddc3a7b4a339182ef84e67f77b58c27b057eb
Justin Chen <ariesblueboy@gmail.com>
2018-07-22 13:47:47 +0800

    link category and restaurant model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,5 @@
 class Restaurant < ApplicationRecord
   mount_uploader :image, PhotoUploader
   validates_presence_of :name, :tel, :address, :opening_hours, :description
+  belongs_to :category, optional: true
 end
