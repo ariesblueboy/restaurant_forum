58f934f0481c9b52ae8a554af477b49d1ad01c52
Justin Chen <ariesblueboy@gmail.com>
2018-08-16 10:51:11 +0800

    user can add comment on restaurant show page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,15 @@
 class CommentsController < ApplicationController
+  def creat
+    @restaurant = Restaurant.find(params[:restaurant_id]) 
+    @comment = @restaurant.comment.build(comment_params) 
+    @comment.user = current_user
+    @commet.save!
+    redirect_to restaurant_path(@restaurant)
+  end
+  
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
 end
